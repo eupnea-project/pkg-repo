e6588f51a3b172568667c9cac881be04eb1d964a
Apacelus <46828271+apacelus@users.noreply.github.com>
2022-12-08 20:58:42 +0100

    Add arch repo scripts

---
+++ create-repo.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# This script creates an arch repository layout in the current directory.
+set -e
+
+# Dump gpg private key into a file
+echo "$private_key" >gpg-private-key.gpg
+
+# import gpg private key
+gpg --import gpg-private-key.gpg
+# Dump public key into a file
+echo "$public_key" >public_key.gpg
+
+# Create dirs
+mkdir -p repo-out/eupnea-utils/os/any
+cd repo-out/eupnea-utils/os/any
+
+# Copy packages
+cp -r ../../../../*.pkg.tar.gz .
+# Sign packages
+gpg --output eupnea-utils.pkg.tar.gz.sig --detach-sig eupnea-utils.pkg.tar.gz
+
+# Create repo and sign it
+repo-add -s ./eupnea-utils.db.tar.gz *.pkg.tar.gz
