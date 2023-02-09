acf189972e6e493e559e7e27ec40b01ecf26761d
Apacelus <46828271+apacelus@users.noreply.github.com>
2023-02-09 20:44:15 +0100

    Fix package wildcard signing

--- create-repo.sh
+++ create-repo.sh
@@ -19,16 +19,7 @@ cp -r ../../*.pkg.tar.gz .
 # Sign packages
 echo "Signing packages"
 # For some reason a eupnea* wildcard doesnt sign all packages
-gpg --detach-sig eupnea-mainline-kernel*.pkg.tar.gz || true
-gpg --detach-sig eupnea-mainline-kernel-headers*.pkg.tar.gz || true
-gpg --detach-sig eupnea-mainline-kernel-modules*.pkg.tar.gz || true
-
-gpg --detach-sig eupnea-chromeos-kernel*.pkg.tar.gz || true
-gpg --detach-sig eupnea-chromeos-kernel-headers*.pkg.tar.gz || true
-gpg --detach-sig eupnea-chromeos-kernel-modules*.pkg.tar.gz || true
-
-gpg --detach-sig eupnea-system*.pkg.tar.gz || true
-gpg --detach-sig eupnea-utils*.pkg.tar.gz || true
+for package in eupnea-*.pkg.tar.gz; do gpg --detach-sig $package; done || true
 gpg --detach-sig cgpt-vboot-utils*.pkg.tar.gz || true
 
 # Create repo and sign it
